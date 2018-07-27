424815ad617bafef345c9b838a03b82e11dd82e1
Mena-Osorio-Christian <christianmena03@gmail.com>
2018-07-27 01:17:03 -0600

    Initial commit

---
+++ src/main/java/hibernate/Categories.java
@@ -0,0 +1,55 @@
+package hibernate;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="CATEGORIES")
+
+public class Categories {
+    @Id
+    @GeneratedValue
+    @Column(name="categoryID")
+    private Long categoryId;
+
+    @Column(name="categoryName")
+    private String categoryName;
+
+    @OneToMany(mappedBy="categories")
+    private Set<Products> products;
+
+    public Categories(Long categoryId, String categoryName,
+                      Set<Products> products) {
+        super();
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.products = products;
+    }
+
+    private Long getCategoryId() {
+        return categoryId;
+    }
+
+    private void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    private String getCategoryName() {
+        return categoryName;
+    }
+
+    private void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    private Set<Products> getProducts() {
+        return products;
+    }
+
+    private void setProducts(Set<Products> products) {
+        this.products = products;
+    }
+
+
+
+}
---
+++ src/main/java/hibernate/Main.java
@@ -0,0 +1,16 @@
+package hibernate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //SessionFactory sf = HibernateUtil.getSessionFactory();
+        //Session session = sf.openSession();
+//        session.beginTransaction();
+//
+//
+//
+//        session.getTransaction().commit();
+//        session.close();
+    }
+}
